c59cccb6552ad81bc3a6ae7417a00cc06da31d92
kim1037 <pan913063@gmail.com>
2023-02-13 00:35:53 +0800

    finished search function

--- find-someone.js
+++ find-someone.js
@@ -5,11 +5,14 @@ const USERS_PER_PAGE = 10
 let usersList = []
 
 const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input') 
+const genderSelect = document.querySelector('#gender-select') 
 
 function renderUserList(users) {
   let rawHTML = ''
   for (let user of users) {
-    rawHTML += `<div class="card mt-3 pt-3 d-flex align-items-center shadow p-3 bg-body rounded" style="width: 16rem;">
+    rawHTML += `<div class="card mt-3 ms-2 pt-3 d-flex align-items-center shadow p-3 bg-body rounded" style="width: 16rem;">
         <img src="${user.avatar}"          
             class="card-img-top user-avatar border rounded-circle shadow-sm bg-body rounded" 
             alt="avatar"
@@ -29,7 +32,7 @@ function renderUserList(users) {
 
   dataPanel.innerHTML = rawHTML
 }
-
+//判斷card上面 姓名前的gender icon是男還是女
 function showGenderIcon(gender) {
   if (gender.toLowerCase() === 'male') {
     return '<i class="fa-solid fa-mars"></i>'
@@ -59,6 +62,24 @@ dataPanel.addEventListener('click', function onAvatarClciked(event){
   if (event.target.matches('.user-avatar')){
     showUserModal(Number(event.target.dataset.id))
   }
+})
+
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+  event.preventDefault()
+  const keyword = searchInput.value.trim().toLowerCase()
+  let filteredUsers = []
+  const gender = Number(genderSelect.value) // 0=male, 1=female, 2=all
+  filteredUsers = usersList.filter((user) => user.region.toLowerCase().includes(keyword))
+  if (gender === 0){
+    filteredUsers = filteredUsers.filter((user)=> user.gender === 'male')
+  } else if (gender === 1) {
+    filteredUsers = filteredUsers.filter((user) => user.gender === 'female')
+  }
+  
+  if (filteredUsers.length === 0){
+    return alert(`Can't find the member from the region: ${keyword}.`)
+  }
+  renderUserList(filteredUsers)
 })
 
 axios.get(INDEX_URL).then(response => {
